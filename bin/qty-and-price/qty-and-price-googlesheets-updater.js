6438b8e03c210dcfa5b37645d7e3825e6a181c49
abdullahaslam306 <f178135@nu.edu.pk>
2022-02-28 16:21:38 +0500

    added update method in google-sheets file and changed method name to bulkUpdate

--- bin/qty-and-price/qty-and-price-googlesheets-updater.js
+++ bin/qty-and-price/qty-and-price-googlesheets-updater.js
@@ -89,7 +89,7 @@ module.exports = class QtyAndPriceGooglesheetsUpdater {
          dbWrite.push(this.createDBRequest(nextToUpdate));   
       })
 
-      await this.sourceSheet.update(updateRequests, 'Batch Update');
+      await this.sourceSheet.bulkUpdate(updateRequests, 'Batch Update');
     }
     await this.QtyAndPrice.bulkWrite(dbWrite);
   }
